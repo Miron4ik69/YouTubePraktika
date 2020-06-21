a3c9a494924d287479553e09f72d078d04c0014d
miron4ik <prihodkovladi19@gmail.com>
2020-06-21 16:23:25 +0300

    update

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,27 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Favorite;
-use Illuminate\Http\Request;
-use App\SearchModel;
 use App\QuerySearch;
+use Illuminate\Http\Request;
 use Facade\Ignition\QueryRecorder\Query;
+use Google_Client;
+use Google_Service;
+use Google_Service_YouTube;
 use Illuminate\Support\Facades\Auth;
 
 class SearchController extends Controller
 {
 
-    public function search() //Search controller
-    {
-        $array = [
-            'title' => $_POST['title'],
-            'id' => 0
-        ];
+    private $youtube;
 
-        function insertQuery($items) // insert Query to db
+    public function __construct()
+    {
+        $client = new Google_Client();
+        $client->setApplicationName(config('google.application_name'));
+        $client->setDeveloperKey(config('google.google_key'));
+        $this->youtube = new Google_Service_YouTube($client);
+    }
+
+    public function search(Request $request)
+    {        
+
+        $inputTitle = $request->input('title');
+
+        function insertQuery($items, $inputTitle) // insert Query to db
         {
             foreach($items->items as $item){
                 QuerySearch::insert([
-                    'query' => $_POST['title'],
+                    'query' => $inputTitle,
                     'video_id' => $item->id->videoId,
                     'video_title' => $item->snippet->title,
                     'video_description' => $item->snippet->description,
@@ -34,14 +44,9 @@ class SearchController extends Controller
             }     
         } 
 
-        $resultsDB = QuerySearch::where('query', $array['title'])->exists(); //check for writing to the db
-               
-        if($resultsDB == null)
-        {         
-            $resultsJson = SearchModel::RequestUrl($array);  
-            insertQuery($resultsJson); 
-
-            foreach($resultsJson->items as $res){  
+        function getArray($result)
+        {
+            foreach($result->items as $res){  
 
                 $arr[] = (object)[
                     'video_id' => $res->id->videoId,
@@ -53,58 +58,39 @@ class SearchController extends Controller
                 ];
             }
 
-            return view('result', ['results' => $arr]); 
+            return $arr;
+        }
 
-        }else {   
-            $resultsDB = QuerySearch::select()->where('query', $array['title'])->get();
-            return view('result', ['results' => $resultsDB]);
-         }
-    }
+        $resultsDB = QuerySearch::where([
+            'query' => $inputTitle
+        ])->exists(); 
 
+        if($resultsDB == null){
 
-    
-
-    public function show($id) // show controller
-    { 
-
-        $array = [
-            'title' => "",
-            'id' => $id
-        ];
-
-        $resultDB = QuerySearch::where([ //check for writing to the db
-            "video_id" => $id
-        ])->exists();
-
-        if($resultDB == null) // if null
-        {
-            $results = SearchModel::RequestUrl($array)->items; //Search in api
-
-            $arr[] = (object)[                                 //get object Array
-                'video_id' => $results[0]->id,
-                'video_title' => $results[0]->snippet->title,
-                'video_description' => $results[0]->snippet->description,
-                'video_image' => $results[0]->snippet->thumbnails->high->url
+            $queryParams = [
+                'order' => 'searchSortUnspecified',
+                'q' => $inputTitle
             ];
 
-            return view('view', ['results' => $arr]); 
+            $response = $this->youtube->search->listSearch('snippet', $queryParams);
 
+            insertQuery($response, $inputTitle);
+
+            return view('result', ['results' => getArray($response)]);
         } else {
-            $resultsDB = QuerySearch::where('video_id', $id)->get();
-    
-            foreach($resultsDB as $result){
-                return view('view', ['results' => $result]);
-            }    
-        }  
+            $resultsDB = QuerySearch::select()->where('query', $inputTitle)->get();
+            return view('result', ['results' => $resultsDB]);
+        }
     }
 
-    public function favoriteadd() // Add to favorite
-    {
-         Favorite::insert([
-            'user_id' => $_POST['user_id'],
-            'video_id' => $_POST['video_id'],
-            'video_title' => $_POST['video_title'],
-            'video_image' => $_POST['video_image']
-         ]);
+        
+    public function show(Request $request, $id) // show controller
+    { 
+        $resultsDB = QuerySearch::where('video_id', $id)->get();
+    
+        foreach($resultsDB as $result){
+            return view('view', ['results' => $result]);
+        }
     }
+
 }
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -84,7 +84,6 @@
   <!-- Infinite Scroll -->
   <script src="https://unpkg.com/infinite-scroll@3/dist/infinite-scroll.pkgd.min.js"></script>
   <!-- Your custom scripts (optional) -->
-  
 
 </body>
 </html>
